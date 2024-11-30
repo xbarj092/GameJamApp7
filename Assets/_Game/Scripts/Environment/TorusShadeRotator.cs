5df19813713ae68056080c3386ce01be4fd35822
Karel Babka <krlbabka@gmail.com>
2024-11-30 10:34:28 +0100

    Added torus and the first shade implementation, Added them to map prefab

---
+++ Assets/_Game/Scripts/Environment/TorusShadeRotator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TorusShadeRotator : MonoBehaviour
+{
+    public Transform player; // Assign the player's transform in the inspector.
+    void Update()
+    {
+        // Get the player's position relative to the world origin
+        Vector3 playerPosition = player.position;
+
+        // Calculate the opposite position
+        Vector3 oppositeDirection = playerPosition.normalized;
+        Debug.Log(oppositeDirection);
+
+        // Determine the position for the blocker shape
+        Quaternion targetRotation = Quaternion.LookRotation(oppositeDirection, Vector3.up);
+        targetRotation = Quaternion.Euler(-90, targetRotation.eulerAngles.y, targetRotation.eulerAngles.z);
+        transform.rotation = targetRotation;
+    }
+}
