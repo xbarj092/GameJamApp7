0b2bd04459d093cf9220a25feee80e6e917b8c8b
Karel Babka <krlbabka@gmail.com>
2024-11-30 20:30:05 +0100

    Half broken bullshit -> shaders, 1D updates

---
+++ Assets/_Game/Scripts/Player/PlayerMovement1D.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement1D : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float moveSpeed = 5f;
+    float input;
+
+    void Update()
+    {
+        input = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        MoveOnLine(input);
+
+    }
+
+    private void MoveOnLine(float input)
+    {
+        Vector3 direction = (pointB.position - pointA.position).normalized;
+
+        Vector3 movement = direction * input * moveSpeed * Time.deltaTime;
+
+        Vector3 targetPosition = transform.position + movement;
+
+        Vector3 lineDirection = (pointB.position - pointA.position).normalized;
+        Vector3 pointToTarget = targetPosition - pointA.position;
+        float projection = Vector3.Dot(pointToTarget, lineDirection);
+        targetPosition = pointA.position + lineDirection * projection;
+
+        transform.position = targetPosition;
+    }
+}
